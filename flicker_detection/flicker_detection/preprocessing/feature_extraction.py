bfc5ce7bb59ce17ce859f05ee972c5f0bf239540
hc07180011 <hendry0718@gmail.com>
2021-07-27 23:46:11 +0800

    fix gif error

--- flicker_detection/flicker_detection/preprocessing/feature_extraction.py
+++ flicker_detection/flicker_detection/preprocessing/feature_extraction.py
@@ -57,13 +57,13 @@ class Features:
             #     facenet.get_embedding, processing_frames[309], processing_frames[310])
             """ Pixel-wise distance example, pass now, video 001: 0, 1, 105, 106
             """
-            pixel = Pixel()
-            for i in range(len(processing_frames) - 1):
-                pixel.get_heatmap(
-                    processing_frames[i], processing_frames[i+1], output=True, dump_dir="./dump")
-            from util.gif import gen_gif_temp
-            gen_gif_temp("dump")
-            exit()
+            # pixel = Pixel()
+            # for i in range(len(processing_frames) - 1):
+            #     pixel.get_heatmap(
+            #         processing_frames[i], processing_frames[i+1], output=True, dump_dir="./dump")
+            # from util.gif import gen_gif_temp
+            # gen_gif_temp("dump")
+            # exit()
 
             logging.info("Start embedding ...")
             facenet = Facenet()
