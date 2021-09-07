b74630d4329fa8e831023d1568e37d93d1a1a90e
hc07180011 <hendry0718@gmail.com>
2021-09-07 18:24:26 +0800

    change some styling

--- flicker_detection/flicker_detection/main.py
+++ flicker_detection/flicker_detection/main.py
@@ -17,24 +17,46 @@ from core.flicker import Flicker
 def main() -> None:
 
     parser = ArgumentParser()
-    parser.add_argument("-d", "--data_path", type=str,
-                        default="data/177193533.mp4", help="path to video, run Flask if empty")
-    parser.add_argument("-l", "--log_dir", type=str, default=".log",
-                        help="directory of log files")
-    parser.add_argument("-dc", "--disable_cache", type=bool, default=False,
-                        help="whether to disable cache function")
-    parser.add_argument("-c", "--cache_dir", type=str, default=".cache",
-                        help="directory of caches")
-    parser.add_argument("-v", "--verbose", type=bool,
-                        default=True, help="whether to print debugging logs")
+    parser.add_argument(
+        "-d", "--data_path", type=str,
+        default="data/177193533.mp4",
+        help="path to video, run Flask if empty"
+    )
+    parser.add_argument(
+        "-l", "--log_dir", type=str,
+        default=".log",
+        help="directory of log files"
+    )
+    parser.add_argument(
+        "-i", "--img_dir", type=str,
+        default=".img",
+        help="directory of experiments .png"
+    )
+    parser.add_argument(
+        "-dc", "--disable_cache", action="store_true",
+        default=False,
+        help="disable caching function"
+    )
+    parser.add_argument(
+        "-c", "--cache_dir", type=str,
+        default=".cache",
+        help="directory of caches"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true",
+        default=False,
+        help="print debugging logs"
+    )
     args = parser.parse_args()
 
     logger = logging.getLogger()
 
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        "%(asctime)s %(name)s[%(process)d] %(levelname)s %(processName)s(%(threadName)s) %(module)s:%(lineno)d  %(message)s",
-        datefmt='%Y%m%d %H:%M:%S')
+        "%(asctime)s %(name)s[%(process)d]%(levelname)s " + 
+        "%(processName)s(%(threadName)s) %(module)s:%(lineno)d  %(message)s",
+        datefmt='%Y%m%d %H:%M:%S'
+    )
 
     ch = logging.StreamHandler()
     if args.verbose:
@@ -44,9 +66,13 @@ def main() -> None:
     ch.setFormatter(formatter)
 
     os.makedirs(args.log_dir, exist_ok=True)
+    os.makedirs(args.img_dir, exist_ok=True)
     os.makedirs(args.cache_dir, exist_ok=True)
+
     log_filename = os.path.join(
-        args.log_dir, datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S.log"))
+        args.log_dir,
+        datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S.log")
+    )
     fh = logging.FileHandler(log_filename)
     if args.verbose:
         fh.setLevel(logging.DEBUG)
@@ -60,20 +86,37 @@ def main() -> None:
     logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
     coloredlogs.install(
-        fmt="%(asctime)s %(hostname)s %(name)s[%(process)d] %(levelname)s %(processName)s(%(threadName)s) %(module)s:%(lineno)d  %(message)s", level="DEBUG")
+        fmt="%(asctime)s %(hostname)s %(name)s[%(process)d] " +
+        "%(levelname)s %(processName)s(%(threadName)s) " +
+        "%(module)s:%(lineno)d  %(message)s",
+        level="DEBUG"
+    )
 
     logging.info("Program starts")
 
     facenet = Facenet()
-    video_features = Features(facenet, args.data_path,
-                              not args.disable_cache, args.cache_dir)
+    video_features = Features(
+        facenet,
+        args.data_path,
+        not args.disable_cache,
+        args.cache_dir
+    )
     video_features.feature_extraction()
 
-    flicker = Flicker(video_features.fps, video_features.similarities, video_features.suspects,
-                      video_features.horizontal_displacements, video_features.vertical_displacements)
+    flicker = Flicker(
+        video_features.fps,
+        video_features.similarities,
+        video_features.suspects,
+        video_features.horizontal_displacements,
+        video_features.vertical_displacements
+    )
 
-    flicker.flicker_detection(output=False, output_path="{}.json".format(
-        os.path.basename(args.data_path)))
+    flicker.flicker_detection(
+        output=False,
+        output_path="{}.json".format(
+            os.path.basename(args.data_path)
+        )
+    )
 
 
 def __monitor(pid):
@@ -90,3 +133,4 @@ if __name__ == "__main__":
     p.start()
     main()
     p.kill()
+
