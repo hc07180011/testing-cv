7cd4c9d8f9e71cccf486f68ab5e9370923704226
henrychao <henrychao>
2022-01-19 03:38:53 +0800

    update main function

--- flicker_detection/flicker_detection/main.py
+++ flicker_detection/flicker_detection/main.py
@@ -1,31 +1,23 @@
 import os
 import time
-import psutil
 import logging
-import datetime
-import coloredlogs
+from argparse import ArgumentParser
+
+import cv2
 import numpy as np
 
-from argparse import ArgumentParser
-from multiprocessing import Process
-
+from mypyfunc.logger import init_logger
 from preprocessing.embedding.facenet import Facenet
-from preprocessing.feature_extraction import Features
-from core.flicker import Flicker
+from mypyfunc.keras import InferenceModel
 
 
-def main() -> None:
+def _main() -> None:
 
     parser = ArgumentParser()
     parser.add_argument(
         "-d", "--data_path", type=str,
-        default="data/177193533.mp4",
-        help="path to video, run Flask if empty"
-    )
-    parser.add_argument(
-        "-l", "--log_dir", type=str,
-        default=".log",
-        help="directory of log files"
+        default="data/flicker-detection/0003.mp4",
+        help="Path to the video"
     )
     parser.add_argument(
         "-i", "--img_dir", type=str,
@@ -49,89 +41,39 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    logger = logging.getLogger()
-
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s %(name)s[%(process)d]%(levelname)s " +
-        "%(processName)s(%(threadName)s) %(module)s:%(lineno)d  %(message)s",
-        datefmt='%Y%m%d %H:%M:%S'
-    )
-
-    ch = logging.StreamHandler()
-    if args.verbose:
-        ch.setLevel(logging.DEBUG)
-    else:
-        ch.setLevel(logging.INFO)
-    ch.setFormatter(formatter)
-
-    os.makedirs(args.log_dir, exist_ok=True)
-    os.makedirs(args.img_dir, exist_ok=True)
-    os.makedirs(args.cache_dir, exist_ok=True)
-
-    log_filename = os.path.join(
-        args.log_dir,
-        datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S.log")
-    )
-    fh = logging.FileHandler(log_filename)
-    if args.verbose:
-        fh.setLevel(logging.DEBUG)
-    else:
-        fh.setLevel(logging.INFO)
-    fh.setFormatter(formatter)
-
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-
-    logging.getLogger('matplotlib').setLevel(logging.WARNING)
-
-    coloredlogs.install(
-        fmt="%(asctime)s %(hostname)s %(name)s[%(process)d] " +
-        "%(levelname)s %(processName)s(%(threadName)s) " +
-        "%(module)s:%(lineno)d  %(message)s",
-        level="DEBUG"
-    )
-
+    init_logger()
     logging.info("Program starts")
 
-    facenet = Facenet()
-    video_features = Features(
-        facenet,
-        args.data_path,
-        args.img_dir,
-        not args.disable_cache,
-        args.cache_dir
-    )
-    video_features.feature_extraction()
-
-    flicker = Flicker(
-        video_features.fps,
-        video_features.similarities,
-        video_features.suspects,
-        video_features.horizontal_displacements,
-        video_features.vertical_displacements,
-        args.img_dir
-    )
-
-    flicker.flicker_detection(
-        output=False,
-        output_path="{}.json".format(
-            os.path.basename(args.data_path)
+    def _get_chunk_array(input_arr: np.array, chunk_size: int) -> list:
+        asymmetric_chunks = np.split(
+            input_arr,
+            list(range(
+                chunk_size,
+                input_arr.shape[0] + 1,
+                chunk_size
+            ))
         )
-    )
+        # TODO: should we take the last chunk?
+        return np.array(asymmetric_chunks[:-1])
 
+    vidcap = cv2.VideoCapture(args.data_path)
+    success, image = vidcap.read()
+    raw_images = list()
+    while success:
+        raw_images.append(cv2.resize(image, (200, 200)))
+        success, image = vidcap.read()
 
-# def __monitor(pid):
-#     memory_usage_gb = []
-#     while True:
-#         memory_usage_gb.append(psutil.Process(pid).memory_info().rss / 2 ** 30)
-#         np.save("memory_record", memory_usage_gb)
-#         time.sleep(1)
+    facenet = Facenet()
+    embeddings = facenet.get_embedding(np.array(raw_images))
+
+    chunk_size = 30
+    X_test = _get_chunk_array(embeddings, chunk_size)
+
+    model = InferenceModel("model.h5")
+    y_pred = model.predict(X_test.reshape(-1, chunk_size, np.prod(X_test.shape[2:])))
+
+    print(y_pred)
 
 
 if __name__ == "__main__":
-    # pid = os.getpid()
-    # p = Process(target=__monitor, args=(pid,))
-    # p.start()
-    main()
-    # p.kill()
+    _main()
