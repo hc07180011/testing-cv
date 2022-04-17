fba486faaa4bcab658c461ac163718f0ddae9781
Tony363 <pysolver33@gmail.com>
2022-04-17 23:45:40 +0800

    separate embedding and train

--- flicker_detection/flicker_detection/mypyfunc/seed.py
+++ flicker_detection/flicker_detection/mypyfunc/seed.py
@@ -34,6 +34,9 @@ def seedEverything(seed=DEFAULT_RANDOM_SEED):
 
 
 def reset_random_seeds(seed=12345):
+    """
+    https://stackoverflow.com/questions/60058588/tesnorflow-2-0-tf-random-set-seed-not-working-since-i-am-getting-different-resul
+    """
     import os
     # *IMPORANT*: Have to do this line *before* importing tensorflow
     os.environ['PYTHONHASHSEED'] = str(seed)
