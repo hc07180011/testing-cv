22699916df8bfcfecdd83e5f8df21f2a5260456b
Tony363 <pysolver33@gmail.com>
2022-04-29 03:58:31 +0800

    serialized each video into batches in TFRecords

--- flicker_detection/flicker_detection/extract_embedding.py
+++ flicker_detection/flicker_detection/extract_embedding.py
@@ -3,7 +3,8 @@ import cv2
 import tqdm
 import logging
 import numpy as np
-from preprocessing.embedding.backbone import BaseCNN
+import tensorflow as tf
+from preprocessing.embedding.backbone import BaseCNN, Serializer
 from tensorflow.keras.applications import resnet, mobilenet, vgg16, InceptionResNetV2, InceptionV3
 from mypyfunc.logger import init_logger
 
@@ -18,7 +19,7 @@ def _embed(
 ) -> None:
     os.makedirs(output_dir, exist_ok=True)
 
-    feature_extractor = BaseCNN()
+    feature_extractor, serializer = BaseCNN(), Serializer()
     # just change extractor to try different
     feature_extractor.adaptive_extractor(mobilenet.MobileNet, frequency=10)
 
@@ -30,31 +31,32 @@ def _embed(
         success, image = vidcap.read()
 
         batched_image = np.zeros((batch_size+1,) + image.shape)
-        frame_count, embeddings = 0, ()
-        batched_image[frame_count] = image
+        batched_image[0] = image
+        frame_count, n_batch = 1, 0
         while success:
             if frame_count == batch_size:
-                embeddings = embeddings + \
-                    tuple(feature_extractor.get_embedding(
-                        batched_image, batched=True).flatten())
-                frame_count = 0
+                serializer.parse_batch(feature_extractor.get_embedding(
+                    batched_image, batched=True).flatten(), n_batch=n_batch, filename=path)
+                frame_count, n_batch = 0, n_batch+1
                 batched_image = np.zeros((batch_size+1,) + image.shape)
 
             success, batched_image[frame_count+1] = vidcap.read()
             frame_count += int(success)
 
-        embeddings = np.array(embeddings)
-
-        np.save(os.path.join(output_dir, path), embeddings)
+        serializer.write_to_tfr()
+        serializer.done_writing()
+        logging.info("done extracting - {}".format(path))
 
 
 def main():
+    tf.keras.utils.set_random_seed(12345)
+    tf.config.experimental.enable_op_determinism()
     init_logger()
 
     logging.info("[Embedding] Start ...")
     _embed(
         os.path.join(data_base_dir, "flicker-detection"),
-        os.path.join(data_base_dir, "")
+        os.path.join(data_base_dir, "embedding")
     )
     logging.info("[Embedding] done.")
 
