45f290d391dd344503ec20fe40f41b5b5d936038
Tony363 <pysolver33@gmail.com>
2022-05-01 01:02:26 +0800

    temp transformers & TFrecord batching

---
+++ flicker_detection/flicker_detection/mypyfunc/transformers.py
@@ -0,0 +1,68 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+
+
+class PositionalEmbedding(layers.Layer):
+    def __init__(self, sequence_length, output_dim, **kwargs):
+        super().__init__(**kwargs)
+        self.position_embeddings = layers.Embedding(
+            input_dim=sequence_length, output_dim=output_dim
+        )
+        self.sequence_length = sequence_length
+        self.output_dim = output_dim
+
+    def call(self, inputs):
+        # The inputs are of shape: `(batch_size, frames, num_features)`
+        length = tf.shape(inputs)[1]
+        positions = tf.range(start=0, limit=length, delta=1)
+        embedded_positions = self.position_embeddings(positions)
+        return inputs + embedded_positions
+
+    def compute_mask(self, inputs, mask=None):
+        mask = tf.reduce_any(tf.cast(inputs, "bool"), axis=-1)
+        return mask
+
+
+class TransformerEncoder(layers.Layer):
+    def __init__(self, embed_dim, dense_dim, num_heads, **kwargs):
+        super().__init__(**kwargs)
+        self.embed_dim = embed_dim
+        self.dense_dim = dense_dim
+        self.num_heads = num_heads
+        self.attention = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim, dropout=0.3
+        )
+        self.dense_proj = tf.keras.Sequential(
+            [layers.Dense(dense_dim, activation=tf.nn.gelu),
+             layers.Dense(embed_dim), ]
+        )
+        self.layernorm_1 = layers.LayerNormalization()
+        self.layernorm_2 = layers.LayerNormalization()
+
+    def call(self, inputs, mask=None):
+        if mask is not None:
+            mask = mask[:, tf.newaxis, :]
+
+        attention_output = self.attention(inputs, inputs, attention_mask=mask)
+        proj_input = self.layernorm_1(inputs + attention_output)
+        proj_output = self.dense_proj(proj_input)
+        return self.layernorm_2(proj_input + proj_output)
+
+
+def transformers(X_train: np.array) -> Model:
+    sequence_length = 20
+    embed_dim = 9216
+    dense_dim = 4
+    num_heads = 1
+    inputs = tf.keras.Input(shape=X_train.shape[1:])
+    x = PositionalEmbedding(
+        sequence_length, embed_dim, name="frame_position_embedding"
+    )(inputs)
+    x = TransformerEncoder(embed_dim, dense_dim, num_heads,
+                           name="transformer_layer")(x)
+    x = layers.GlobalMaxPooling1D()(x)
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    model = tf.keras.Model(inputs, outputs)
+    return model
