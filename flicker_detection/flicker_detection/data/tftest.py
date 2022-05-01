d0d131b89a102806c4c2494c565642b094e50e9d
Tony363 <pysolver33@gmail.com>
2022-05-01 17:03:41 +0800

    need to figure out how to use tensorflow tfrecords

---
+++ flicker_detection/flicker_detection/data/tftest.py
@@ -0,0 +1,34 @@
+import tempfile
+import numpy as np
+import os
+import json
+import tensorflow as tf
+
+# Read the data back out.
+
+
+def decode_fn(record_bytes):
+    jm = json.load(open('test.json', "r"))
+    schema = dict(map(lambda x: (x, tf.io.FixedLenFeature(
+        [], dtype=tf.float32)), jm['data/TFRecords/0099.mp4']))
+    return tf.io.parse_single_example(
+        # Data
+        record_bytes,
+        schema
+        # Schema
+        # {"x": tf.io.FixedLenFeature([], dtype=tf.float32),
+        #  "y": tf.io.FixedLenFeature([], dtype=tf.float32)}
+    )
+
+
+if __name__ == "__main__":
+
+    example_path = "0099.mp4.tfrecords"
+    np.random.seed(0)
+    jm = json.load(open('test.json', "r"))
+    schema = dict(map(lambda x: (x, tf.io.FixedLenFeature(
+        [], dtype=tf.float32)), jm['data/TFRecords/0099.mp4']))
+    ds = tf.data.TFRecordDataset(example_path)
+    # ds.map(decode_fn)
+    data = next(ds.as_numpy_iterator())
+    parsed = tf.io.parse_single_example(data, schema)
