be63a171bf581f57134e69bd06b995cf5b9fb139
Tony363 <pysolver33@gmail.com>
2022-05-02 23:30:54 +0800

    decoded a tfrecord but don't know hwo to use it

--- flicker_detection/flicker_detection/data/tftest.py
+++ flicker_detection/flicker_detection/data/tftest.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 
 # Read the data back out.
 
+import sys
+
 
 def decode_fn(record_bytes):
     jm = json.load(open('test.json', "r"))
@@ -17,16 +19,17 @@ def decode_fn(record_bytes):
 
 
 if __name__ == "__main__":
-
+    f = open("test.out", 'w')
+    sys.stdout = f
     example_path = "0099.mp4.tfrecords"
     np.random.seed(0)
-    jm = json.load(open('test.json', "r"))
-    schema = dict(map(lambda x: (x, tf.io.FixedLenFeature(
-        [], dtype=tf.float32)), jm['data/TFRecords/0099.mp4']))
-    ds = tf.data.TFRecordDataset(example_path)
 
-    # for batch in ds.map(decode_fn):
-    #     print(tf.io.parse_tensor(batch, out_type=tf.double))
-    data = next(ds.as_numpy_iterator())
-    parsed = tf.io.parse_tensor(data, out_type=tf.float32)
+    ds = tf.data.TFRecordDataset(example_path)
+    for batch in ds.map(decode_fn):
+        for key in batch:
+            tensor = tf.io.parse_tensor(batch[key], out_type=tf.float32)
+            print(tensor.numpy())
+    # data = next(ds.as_numpy_iterator())
+    # parsed = tf.io.parse_tensor(data, out_type=tf.float32)
     # print(parsed)
+    f.close()
