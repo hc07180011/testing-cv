eb00714903d6b548e271a620b6c280d7c9a679a0
Tony363 <pysolver33@gmail.com>
2022-05-22 02:30:44 +0800

    time to put into training loop

--- flicker_detection/flicker_detection/mypyfunc/torch_data_loader.py
+++ flicker_detection/flicker_detection/mypyfunc/torch_data_loader.py
@@ -6,51 +6,6 @@ from torch.utils.data import Dataset, DataLoader
 from typing import Tuple
 
 
-def _get_chunk_array(input_arr: np.array, chunk_size: int) -> Tuple:
-    chunks = np.array_split(
-        input_arr,
-        list(range(
-            chunk_size,
-            input_arr.shape[0] + 1,
-            chunk_size
-        ))
-    )
-    i_pad = np.zeros(chunks[0].shape)
-    i_pad[:len(chunks[-1])] = chunks[-1]
-    chunks[-1] = i_pad
-    return tuple(map(tuple, chunks))
-
-
-def load_embeddings(
-    embedding_list_train: list,
-    label_path: str,
-    mapping_path: str,
-    data_dir: str,
-    batch_size: int = 30,
-) -> Tuple[np.ndarray, np.ndarray]:
-    encoding_filename_mapping = json.load(open(mapping_path, "r"))
-    raw_labels = json.load(open(label_path, "r"))
-    X_train, y_train = (), ()
-    for key in embedding_list_train:
-        real_filename = encoding_filename_mapping[key.replace(
-            ".tfrecords", "")]
-        loaded = np.load("{}.npy".format(os.path.join(data_dir, key.replace(
-            ".tfrecords", ""))))
-        X_train += (*_get_chunk_array(loaded, batch_size),)
-        # get flicker frame indexes
-        flicker_idxs = np.array(raw_labels[real_filename]) - 1
-        # buffer zeros array frame video embedding
-        buf_label = np.zeros(loaded.shape[0], dtype=np.uint8)
-        # set indexes in zeros array based on flicker frame indexes
-        buf_label[flicker_idxs] = 1
-        y_train += tuple(
-            1 if sum(x) else 0
-            for x in _get_chunk_array(buf_label, batch_size)
-        )  # consider using tf reduce sum for multiclass
-        # logging.info("Video - {}".format(key))
-    return np.array(X_train), np.array(y_train)
-
-
 class MYDS(Dataset):
     def __init__(
         self,
@@ -58,13 +13,16 @@ class MYDS(Dataset):
         label_path: str,
         mapping_path: str,
         data_dir: str,
+        chunk_size: int = 30,
     ):
+        self.label_path = label_path
+        self.mapping_path = mapping_path
+        self.data_dir = data_dir
         self.x_paths = x_paths
-        self.xs, self.ys = load_embeddings(
-            self.x_paths,
-            label_path=label_path,
-            mapping_path=mapping_path,
-            data_dir=data_dir,
+        self.chunk_size = chunk_size
+
+        self.xs, self.ys = self.load_embeddings(
+            self.x_paths
         )
 
     def __len__(self):
@@ -72,6 +30,46 @@ class MYDS(Dataset):
 
     def __getitem__(self, idx: int):
         return self.xs[idx], self.ys[idx]
+
+    @staticmethod
+    def _get_chunk_array(input_arr: np.array, chunk_size: int) -> Tuple:
+        chunks = np.array_split(
+            input_arr,
+            list(range(
+                chunk_size,
+                input_arr.shape[0] + 1,
+                chunk_size
+            ))
+        )
+        i_pad = np.zeros(chunks[0].shape)
+        i_pad[:len(chunks[-1])] = chunks[-1]
+        chunks[-1] = i_pad
+        return tuple(map(tuple, chunks))
+
+    def load_embeddings(
+        self,
+        embedding_list_train: list,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        encoding_filename_mapping = json.load(open(self.mapping_path, "r"))
+        raw_labels = json.load(open(self.label_path, "r"))
+        X_train, y_train = (), ()
+        for key in embedding_list_train:
+            real_filename = encoding_filename_mapping[key.replace(
+                ".tfrecords", "")]
+            loaded = np.load("{}.npy".format(os.path.join(self.data_dir, key.replace(
+                ".tfrecords", ""))))
+            X_train += (*self._get_chunk_array(loaded, self.chunk_size),)
+            # get flicker frame indexes
+            flicker_idxs = np.array(raw_labels[real_filename]) - 1
+            # buffer zeros array frame video embedding
+            buf_label = np.zeros(loaded.shape[0], dtype=np.uint8)
+            # set indexes in zeros array based on flicker frame indexes
+            buf_label[flicker_idxs] = 1
+            y_train += tuple(
+                1 if sum(x) else 0
+                for x in self._get_chunk_array(buf_label, self.chunk_size)
+            )  # consider using tf reduce sum for multiclass
+        return np.array(X_train), np.array(y_train)
 
 
 if __name__ == '__main__':
