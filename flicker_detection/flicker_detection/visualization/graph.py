af7a3846721ef4c0f9d79bd920a818fd46a10eb4
Sebastian <sebastil@hotmail.com>
2022-05-23 23:27:39 +0800

    added mean shift and refined the graphs

--- flicker_detection/flicker_detection/visualization/graph.py
+++ flicker_detection/flicker_detection/visualization/graph.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objects as go
 
-def graphKmeans(dataSet, labels, unique_labels):
+def graph(dataSet, labels, unique_labels):
     # f = go.FigureWidget()
     # f.layout.hovermode = 'closest'
     # f.layout.hoverdistance = -1
@@ -15,7 +15,7 @@ def graphKmeans(dataSet, labels, unique_labels):
     for i in unique_labels:
         plt.scatter(dataSet[labels == i , 0] , dataSet[labels == i , 1] , label = i)
     plt.legend()
-    plt.savefig('myresult.svg')
-    plt.savefig('myresult.png')
-    plt.savefig('myresult.pdf')
+    plt.savefig('./visualization/myresult.svg')
+    plt.savefig('./visualization/myresult.png')
+    plt.savefig('./visualization/myresult.pdf')
     return
