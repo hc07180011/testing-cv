b238cec534b0b4a69f5dd63ca3e1e30748de2199
Sebastian <sebastil@hotmail.com>
2022-05-25 12:03:25 +0800

    changed graphing

--- flicker_detection/flicker_detection/visualization/meanShift.py
+++ flicker_detection/flicker_detection/visualization/meanShift.py
@@ -2,12 +2,12 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 import numpy as np
 from visualization.graph import graph
 
-def meanshift(dataSet):
-    bandwidth = estimate_bandwidth(dataSet, quantile=0.5, n_samples=1000)
+def meanshift(model_result, dataSet):
+    bandwidth = estimate_bandwidth(dataSet, quantile=0.3, n_samples=800)
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(dataSet)
     labels = ms.labels_
     cluster_centers = ms.cluster_centers_
     unique_labels = np.unique(labels)
     print("{} unique clusters found".format(len(unique_labels)))
-    graph(dataSet, labels, unique_labels)
+    graph(model_result, labels, unique_labels)
     return
