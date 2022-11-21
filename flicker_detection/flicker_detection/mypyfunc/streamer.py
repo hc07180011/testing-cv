still havn't found source of bug but no vanishing gradient problem

flicker_detection/flicker_detection/mypyfunc/streamer.py
@@ -1,5 +1,6 @@
 import os
 import random
+import json
 import itertools
 import tqdm
 import numpy as np
@@ -12,96 +13,52 @@ from torchvision.datasets.folder import make_dataset
 from typing import Tuple, Callable
 
 
-def _find_classes(dir):
-    classes = [d.name for d in os.scandir(dir) if d.is_dir()]
-    classes.sort()
-    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
-    return classes, class_to_idx
-
-
-def get_samples(root, extensions=(".mp4", ".avi")):
-    _, class_to_idx = _find_classes(root)
-    return make_dataset(root, class_to_idx, extensions=extensions)
-
-
-class RandomDataset(torch.utils.data.IterableDataset):
-    """
-    https://pytorch.org/vision/main/auto_examples/plot_video_api.html
-    """
-
-    def __init__(self, root, epoch_size=None, frame_transform=None, video_transform=None, clip_len=16):
-        super(RandomDataset).__init__()
-        self.samples = get_samples(root)
-
-        # Allow for temporal jittering
-        if epoch_size is None:
-            epoch_size = len(self.samples)
-        self.epoch_size = epoch_size
-
-        self.clip_len = clip_len
-        self.frame_transform = frame_transform
-        self.video_transform = video_transform
-
-    def __iter__(self):
-        for i in range(self.epoch_size):
-            # Get random sample
-            path, target = random.choice(self.samples)
-            # Get video object
-            vid = torchvision.io.VideoReader(path, "video")
-            metadata = vid.get_metadata()
-            video_frames = []  # video frame buffer
-
-            # Seek and return frames
-            max_seek = metadata["video"]['duration'][0] - \
-                (self.clip_len / metadata["video"]['fps'][0])
-            start = random.uniform(0., max_seek)
-            for frame in itertools.islice(vid.seek(start), self.clip_len):
-                video_frames.append(self.frame_transform(frame['data']))
-                current_pts = frame['pts']
-            # Stack it into a tensor
-            video = torch.stack(video_frames, 0)
-            if self.video_transform:
-                video = self.video_transform(video)
-            output = {
-                'path': path,
-                'video': video,
-                'target': target,
-                'start': start,
-                'end': current_pts}
-            yield output
-
-
 class VideoDataSet(IterableDataset):
     def __init__(
         self,
         vid_lst: list,
+        labels: dict,
         class_size: int,
         oversample: bool,
-        undersample: int,
+        undersample: int = 0,
     ) -> None:
-        self.vid_lst = vid_lst
+        self.__vid_lst = vid_lst
+        self.__labels = labels
         self.batch_size = class_size
         self.oversample = oversample
         self.undersample = undersample
 
+    def len_videos(self)->int:
+        return len(self.__vid_lst)
+
     @property
     def shuffled_data_list(self):
-        return random.sample(self.vid_lst, len(self.vid_lst))
+        return random.sample(self.__vid_lst, len(self.__vid_lst))
 
     @staticmethod
-    def _load(vid: str) -> np.ndarray:
-        yield skvideo.io.vread(vid)
+    def __load(vid: str, label: int = -1) -> np.ndarray:
+        video = skvideo.io.vread(vid)
+        if label is not None and label < 0:
+            yield video
+            return
+        pad = np.zeros((video.shape[0]+1, *video.shape[1:]))
+        pad[-1].fill(label if label is not None else int(bool(label)))
+        yield pad
 
-    def _get_stream(self, vid_lst: list,) -> itertools.chain.from_iterable:
+    def __get_stream(self, vid_lst: list,) -> itertools.chain.from_iterable:
+        if not self.oversample and not self.undersample:
+            mapping = map(lambda f: self.__load(
+                f, self.__labels.get(f.split("/", 3)[-1].replace(".mp4", ""))), vid_lst)
+            return itertools.chain.from_iterable(mapping)
         if self.oversample:
             vid_lst = itertools.cycle(vid_lst)
         elif self.undersample:
             vid_lst = random.sample(vid_lst, self.undersample)
-        return itertools.chain.from_iterable(map(self._load, vid_lst))
+        return itertools.chain.from_iterable(map(self.__load, vid_lst))
 
-    def _get_streams(self) -> zip:
+    def __get_streams(self) -> zip:
         return zip(*[
-            self._get_stream(self.shuffled_data_list)
+            self.__get_stream(self.shuffled_data_list)
             for _ in range(self.batch_size)
         ])
 
@@ -109,6 +66,7 @@ class VideoDataSet(IterableDataset):
     def split_datasets(
         cls: IterableDataset,
         vid_lst: list,
+        labels: dict,
         class_size: int,
         max_workers: int,
         oversample: bool = False,
@@ -121,73 +79,155 @@ class VideoDataSet(IterableDataset):
         split_lst = np.array_split(np.array(vid_lst), num_workers)
         return [cls(
             lst.tolist(),
+            labels=labels,
             class_size=class_size,
             oversample=oversample,
             undersample=undersample)
             for lst in split_lst]
 
     def __iter__(self) -> itertools.chain.from_iterable:
-        return self._get_streams()
+        return self.__get_streams()
 
 
 class MultiStreamer(object):
     """
     https://medium.com/speechmatics/how-to-build-a-streaming-dataloader-with-pytorch-a66dd891d9dd
-    TO DO implement multiprocess labeling from the DataLoader class 
-    for imbalance class and for multiclass
+    TODO merge multiclass binary method
     """
 
     def __init__(
         self,
-        non_flickers: IterableDataset,
-        flickers: IterableDataset,
-        batch_size: int
+        *args: Tuple[IterableDataset],
+        batch_size: int,
+        binary:bool=False,
     ) -> None:
-        self.non_flickers = non_flickers
-        self.flickers = flickers
+        self.balanced = len(args) > 1
         self.batch_size = batch_size
-        self.batch_idx = list(range(batch_size))
+        self.__binary = binary
+        self.__batch_idx = list(range(batch_size))
+
+        self.__datasets = args
+        self.__streams = None
+
+    def get_datasets(self)->list:
+        return self.__datasets
+
+    def __len__(self)->int:
+        return self.__datasets[0][0].undersample if self.balanced else self.__datasets[0][0].len_videos()
 
     @staticmethod
-    def _get_stream_loaders(vid_lst: list):
+    def __get_stream_loaders(vid_lst: list):
         random.shuffle(vid_lst)
         return zip(*[
             DataLoader(ds, num_workers=1, batch_size=None, pin_memory=True)
             for ds in vid_lst
         ])
 
+    @staticmethod
+    def __balanced(
+        streams: zip,
+        idx: list,
+        multiclass: bool = False,
+    ):
+        """
+        USAGE: MULTICLASS
+        batch_size = 5
+        non_flickers = VideoDataSet.split_datasets(
+            non_flicker_files[:8], class_size=1, max_workers=1)  # , undersample=len(flicker_files[:4]))
+        flicker1 = VideoDataSet.split_datasets(
+            flicker1_files[:8], class_size=1, max_workers=1)  # oversample=True)
+        flicker2 = VideoDataSet.split_datasets(
+            flicker2_files[:8], class_size=1, max_workers=1)
+        flicker3 = VideoDataSet.split_datasets(
+            flicker3_files[:8], class_size=1, max_workers=1)
+        flicker4 = VideoDataSet.split_datasets(
+            flicker4_files[:8], class_size=1, max_workers=1)
+        loader = MultiStreamer(non_flickers,
+                            flicker1, flicker2, flicker3, flicker4, batch_size=batch_size)
+        USAGE: BINARY
+        batch_size = 4
+        non_flicker_train = VideoDataSet.split_datasets(
+            non_flicker_train, class_size=batch_size//2, max_workers=1, undersample=len(flicker_train))
+        flicker_train = VideoDataSet.split_datasets(
+            flicker_train, class_size=batch_size//2, max_workers=1)
+        ds_train = MultiStreamer(non_flicker_train, flicker_train, batch_size)
+        USAGE: IMBALANCED
+        non_flickers = VideoDataSet.split_datasets(
+            non_flicker_files[:12], labels=labels, class_size=1, max_workers=8, undersample=0)
+        loader = MultiStreamer(
+            non_flickers, batch_size=batch_size)  
+        """
+        for stream in streams:
+            stream = list(itertools.chain(
+                *tuple(map(lambda s: list(itertools.chain(*s)), stream))
+            ))
+            inputs = torch.stack(stream)
+            if multiclass:
+                labels = torch.arange(len(stream))
+            else:
+                labels = torch.zeros(inputs.shape[0])
+                labels[labels.size(dim=0)//2:] = 1
+            random.shuffle(idx)
+            yield inputs[idx].float(), labels[idx].long()
+
+    @staticmethod
+    def __imbalance(
+        streams: zip,
+        binary:bool
+    ):
+        for stream in streams:
+            frames = [frame[:-1]
+                      for frame in list(itertools.chain(*stream[0]))]
+            labels = [label[-1][0][0][0].item() if not binary else int(bool(label[-1][0][0][0].item()))
+                      for label in list(itertools.chain(*stream[0]))]
+            yield torch.stack(frames).float(), torch.Tensor(labels).long()
+
     def __iter__(self):
-        f_stream, n_stream = self._get_stream_loaders(
-            self.flickers), self._get_stream_loaders(self.non_flickers)
-        for f, n in zip(f_stream, n_stream):
-            random.shuffle(self.batch_idx)
-            f_vid, n_vid = list(itertools.chain(*f)), list(itertools.chain(*n))
-            f_labels, n_labels = torch.ones(
-                len(f_vid)), torch.zeros(len(n_vid))
-            inputs, labels = torch.stack(
-                f_vid+n_vid), torch.cat([f_labels, n_labels])
-            yield inputs[self.batch_idx].float(), labels[self.batch_idx].long()
+        self.__streams = zip(
+            *tuple(map(self.__get_stream_loaders, self.__datasets)))
+        if self.balanced:
+            return self.__balanced(self.__streams, self.__batch_idx, len(self.__datasets) > 2)
+        return self.__imbalance(self.__streams,self.__binary)
 
 
 if __name__ == '__main__':
-    # work with shorter videos with smaller samples to debug
+    non_flicker_dir = "../data/no_flicker"
+    flicker1_dir = "../data/flicker1"
+    flicker2_dir = "../data/flicker2"
+    flicker3_dir = "../data/flicker3"
+    flicker4_dir = "../data/flicker4"
 
-    non_flicker_dir = "../data/meta-data"
-    flicker_dir = "../data/flicker-chunks"
     non_flicker_files = [os.path.join(non_flicker_dir, path)
                          for path in os.listdir(non_flicker_dir)]
-    flicker_files = [os.path.join(flicker_dir, path)
-                     for path in os.listdir(flicker_dir)]
+    flicker1_files = [os.path.join(flicker1_dir, path)
+                      for path in os.listdir(flicker1_dir)]
+    flicker2_files = [os.path.join(flicker2_dir, path)
+                      for path in os.listdir(flicker2_dir)]
+    flicker3_files = [os.path.join(flicker3_dir, path)
+                      for path in os.listdir(flicker3_dir)]
+    flicker4_files = [os.path.join(flicker4_dir, path)
+                      for path in os.listdir(flicker4_dir)]
 
+    labels = json.load(open("../data/multi_label.json", "r"))
     batch_size = 4
     non_flickers = VideoDataSet.split_datasets(
-        non_flicker_files[:12], class_size=batch_size//2, max_workers=1, undersample=len(flicker_files[:4]))
-    flickers = VideoDataSet.split_datasets(
-        flicker_files[:4], class_size=batch_size//2, max_workers=1)  # oversample=True)
+        non_flicker_files[:12], labels=labels, class_size=2, max_workers=1, undersample=12)
+    flicker1 = VideoDataSet.split_datasets(
+        flicker1_files[:4]+flicker4_files[:4], labels=labels, class_size=2, max_workers=1, oversample=True)
+    flicker2 = VideoDataSet.split_datasets(
+        flicker2_files[:8], labels=labels, class_size=1, max_workers=1, oversample=True)
+    flicker3 = VideoDataSet.split_datasets(
+        flicker3_files[:8], labels=labels, class_size=1, max_workers=1, oversample=True)
 
-    loader = MultiStreamer(non_flickers, flickers, batch_size)
+    loader = MultiStreamer(
+        non_flickers, 
+        flicker1,
+        batch_size=batch_size,binary=True)  # , flicker1, flicker2, flicker3,
     for i in range(2):
         print(f"{i} WTF")
-        for inputs, labels in loader:
-            print(inputs.shape, labels)
-            pass
+        temp = torch.zeros((10))
+        for inputs, labels in tqdm.tqdm(loader):
+            print(torch.equal(inputs,temp), labels)
+            temp = inputs
+
+    # test_loader()

flicker_detection/flicker_detection/mypyfunc/torch_models.py
@@ -116,16 +116,6 @@ class CNN_LSTM(nn.Module):
         # initialize weights & bias with stdv -> 0.05
         self.initialization()
 
-        # self.classifier = nn.Sequential(OrderedDict([
-        #     # LSTM Layer
-        #     ('lstm', nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=layer_dim,
-        #                      batch_first=True, bidirectional=bidirectional)),
-        #     # Linear Dense
-        #     ('fc1', nn.Linear(hidden_dim*self.n_directions, hidden_dim//2)),
-        #     # Linear Dense
-        #     ('fc2', nn.Linear(hidden_dim//2, self.output_dim)),
-        # ]))
-
     def init_hidden(self, x: torch.Tensor) -> torch.FloatTensor:
         h0 = torch.zeros(
             self.layer_dim*self.n_directions,
@@ -175,23 +165,38 @@ class CNN_LSTM(nn.Module):
                         nn.init.zeros_(param.data)
 
 
-class ExtractorCNN(nn.Module):
+class OHEMLoss(nn.Module):
     def __init__(
         self,
-        cnn: torchvision.models,
+        batch_size:int,
+        init_epoch:int,
+        criterion:nn.Module,
     ) -> None:
-        super(ExtractorCNN, self).__init__()
-        self.cnn = cnn
+        super(OHEMLoss, self).__init__()
+        self.__batch_size = batch_size
+        self.init_epoch = init_epoch
+        self.criterion = criterion
 
-    def remove_header(self) -> None:
-        self.cnn.classifier = nn.Sequential(
-            *list(self.cnn.classifier.children())[:-3])
 
     def forward(
         self,
-        x: torch.Tensor
+        pred: torch.Tensor,
+        target: torch.Tensor,
+        epoch:int,
     ) -> torch.Tensor:
-        return self.cnn.features(x)
+        if epoch < self.init_epoch:
+            return self.criterion(pred,target)
+            
+        ohem_loss = F.cross_entropy(
+            pred, target, reduction='none', ignore_index=-1)
+        sorted_ohem_loss, idx = torch.sort(ohem_loss, descending=True)
+        keep_num = min(sorted_ohem_loss.size()[0], self.__batch_size)
+
+        if keep_num < sorted_ohem_loss.size()[0]:
+            keep_idx_cuda = idx[:keep_num]
+            ohem_loss = ohem_loss[keep_idx_cuda]
+
+        return ohem_loss.sum() / keep_num
 
 
 """

flicker_detection/flicker_detection/training.py
@@ -12,7 +12,7 @@ from typing import Callable, Tuple
 from argparse import ArgumentParser
 from mypyfunc.logger import init_logger
 from mypyfunc.torch_eval import F1Score, Evaluation
-from mypyfunc.torch_models import CNN_LSTM
+from mypyfunc.torch_models import CNN_LSTM,OHEMLoss
 from mypyfunc.torch_utility import save_checkpoint, save_metrics, load_checkpoint, load_metrics, torch_seeding
 from mypyfunc.streamer import MultiStreamer, VideoDataSet
 
@@ -43,7 +43,7 @@ def training(
             labels = labels.long().to(device)
             # logging.debug(f"{labels}")
             outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels,epoch)
             optimizer.zero_grad()
             loss.backward()
             torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
@@ -68,7 +68,7 @@ def training(
                 labels = labels.long().to(device)
 
                 outputs = model(inputs)
-                loss = criterion(outputs, labels)
+                loss = criterion(outputs, labels,epoch)
                 val_f1 = f1_metric(
                     torch.topk(objective(outputs), k=1, dim=1).indices.flatten(), labels)
                 minibatch_loss_val += loss.item()
@@ -151,11 +151,11 @@ def command_arg() -> ArgumentParser:
                         help='path of json that store the labeled frames')
     parser.add_argument('--flicker_dir', type=str, default="data/flicker-chunks",
                         help='directory of processed flicker video chunks')
-    parser.add_argument('--non_flicker_dir', type=str, default="data/meta-data",
+    parser.add_argument('--non_flicker_dir', type=str, default="data/no_flicker",
                         help='directory of processed non flicker video chunks')
     parser.add_argument('--cache_path', type=str, default=".cache/train_test",
                         help='directory of miscenllaneous information')
-    parser.add_argument('--model_path', type=str, default="model0",
+    parser.add_argument('--model_path', type=str, default="cnn_lstm_model",
                         help='directory to store model weights and bias')
     parser.add_argument(
         "-train", "--train", action="store_true",
@@ -176,9 +176,10 @@ def main() -> None:
 
     __cache__ = np.load(
         "{}.npz".format(cache_path), allow_pickle=True)
-    flicker_train, non_flicker_train, fp_test, flicker_test, non_flicker_test = tuple(
+    flicker_train, non_flicker_train, flicker_test, non_flicker_test = tuple(
         __cache__[lst] for lst in __cache__)
-
+    labels = json.load(open(label_path, 'r'))
+    
     input_dim = 61952
     output_dim = 2
     hidden_dim = 64
@@ -187,7 +188,7 @@ def main() -> None:
     batch_size = 4
 
     model = CNN_LSTM(
-        cnn=torchvision.models.vgg16(pretrained=True),
+        cnn=torchvision.models.vgg19(pretrained=True),
         input_dim=input_dim,
         output_dim=output_dim,
         hidden_dim=hidden_dim,
@@ -198,9 +199,10 @@ def main() -> None:
     model = torch.nn.DataParallel(model)
     model.to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+    optimizer = torch.optim.Adam(
+        model.parameters(),lr=1e-5, weight_decay=1e-6)
     metric = F1Score(average='macro')
-    criterion = nn.CrossEntropyLoss()
+    criterion = OHEMLoss(batch_size=batch_size//2,init_epoch=100,criterion=nn.CrossEntropyLoss())  
     objective = nn.Softmax()
     epochs = 1000
 
@@ -211,11 +213,11 @@ def main() -> None:
         flicker_train = [os.path.join(flicker_path, f)
                          for f in flicker_train]
         non_flicker_train = VideoDataSet.split_datasets(
-            non_flicker_train, class_size=batch_size//2, max_workers=1, undersample=len(flicker_train))
+            non_flicker_train, labels=labels, class_size=batch_size//2, max_workers=1, undersample=1000)
         flicker_train = VideoDataSet.split_datasets(
-            flicker_train, class_size=batch_size//2, max_workers=1)
+            flicker_train,  labels=labels,class_size=batch_size//2, max_workers=1,oversample=True)
 
-        ds_train = MultiStreamer(non_flicker_train, flicker_train, batch_size)
+        ds_train = MultiStreamer(non_flicker_train, flicker_train, batch_size=batch_size)
         logging.info("Done loading training set")
 
         logging.info("Loading validtaion set..")
@@ -224,11 +226,11 @@ def main() -> None:
         flicker_val = [os.path.join(flicker_path, f)
                        for f in flicker_test]
         non_flicker_val = VideoDataSet.split_datasets(
-            non_flicker_val, class_size=batch_size//2, max_workers=1, undersample=len(flicker_val))
+            non_flicker_val, labels=labels, class_size=batch_size//2, max_workers=1, undersample=200)
         flicker_val = VideoDataSet.split_datasets(
-            flicker_val, class_size=batch_size//2, max_workers=1)
+            flicker_val, labels=labels, class_size=batch_size//2, max_workers=1,oversample=True)
 
-        ds_val = MultiStreamer(non_flicker_val, flicker_val, batch_size)
+        ds_val = MultiStreamer(non_flicker_val, flicker_val, batch_size=batch_size)
         logging.info("Done loading validation set")
 
         logging.info(f"{model.train()}")