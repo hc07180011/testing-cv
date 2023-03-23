0d80949395cbda6d640459c473a325b1df41b471
Tony363 <pysolver33@gmail.com>
2023-03-23 12:45:38 +0800

    implemented loss

--- humanoid-torch/modules/losses.py
+++ humanoid-torch/modules/losses.py
@@ -17,33 +17,38 @@ class HeatMapInteractLoss(torch.nn.Module):
     
     def forward(
         self,
-        x:torch.Tensor,
+        heatmap:torch.Tensor,
         heatmap_true:torch.Tensor,
         interact:torch.Tensor,
         interact_true:torch.Tensor,
     )->torch.Tensor:
         hm_loss = self.ce(
-            x.flatten(start_dim=1),
-            heatmap_true.flatten(start_dim=1).repeat(x.shape[0],1)
+            heatmap.flatten(start_dim=1),
+            heatmap_true.flatten(start_dim=1).repeat(heatmap.shape[0],1)
             )
         # predict_hm = self.softmax(heatmap.reshape(-1,np.prod(heatmap.shape[1:]))).reshape(-1,*heatmap.shape[1:],1)
-        print(np.prod(interact_true.shape[1:]))
-        interact_flat = interact_true.flatten(start_dim=1)
+        interact_flat = interact.flatten(start_dim=1)
         interact = self.relu(self.fc(interact_flat))
-        interact_loss = self.ce(interact_true,interact)
+        interact_loss = self.ce(interact,interact_true)
         return  hm_loss + interact_loss
-
-
-
-
 
 
     
 if __name__ == '__main__':
+    '''
+    2023-03-23 12:06:11,157 heatmap loss: nan
+    2023-03-23 12:06:11,320 interact loss: 6.89274e+33
+    2023-03-23 12:06:11,621 total loss: nan
+    2023-03-23 12:06:12,008 Interacts: (7,)
+    2023-03-23 12:06:12,008 Heatmap OUT: (32, 180, 320, 1)
+    2023-03-23 12:06:12,008 Heatmap TRUE: (?, 180, 320, 1)
+    2023-03-23 12:06:12,008 Interact OUT: (32, 6, 10, 1)
+    2023-03-23 12:06:12,008 Interact TRUE: (?, 7)
+    '''
     im_outputs = torch.randn(size=(4,1,180,320))
     hm_target = torch.randn(size=(1,1,180,320))
-    int_out = torch.randn(0,7,size=(1,60))
-    int_target = torch.randint(0,7,size=(1,7))
+    int_out = torch.randn(size=(1,6,10,1),requires_grad=True)
+    int_target = torch.randint(0,7,size=(1,7,)).float()
     criterion = HeatMapInteractLoss()
-    print(criterion(im_outputs,hm_target,int_out))
+    print(criterion(im_outputs,hm_target,int_out,int_target))
     pass
